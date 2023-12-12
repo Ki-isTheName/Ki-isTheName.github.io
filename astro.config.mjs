56da5742d0f84567cc3fddf6d3718745deab8d53
Ki-isTheName <bidguru94@gmail.com>
2023-12-12 23:29:14 +0800

    connect to storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
-
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
 import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr", "my"],
@@ -12,5 +21,26 @@ export default defineConfig({
     }
   },
   site: 'https://zakizakaria-cybersec.github.io',
-  integrations: [tailwind()]
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: 'storyblok/Page',
+        config: 'storyblok/Config',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+      },
+    }),
+    tailwind()
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+  apiOptions: {
+    region: "eu",
+  },
 });
